db125984705cb0e2d7f4a203d33204d29c07056e
Priya Ranjan Kumar <ranjanrana1712@gmail.com>
2023-11-25 00:14:26 +0530

    Update script.js

--- script.js
+++ script.js
@@ -9,17 +9,15 @@ const hourHand = document.querySelector('#hour-hand');
             // the new keyword is used to create an instance of an object that is defined by a constructor function.
             const now = new Date();
             const seconds = now.getSeconds();
-            // const secondDegree = ((seconds/60)*360) + 90;
             const secondDegree = seconds*6;
             secondHand.style.transform = `rotate(${secondDegree}deg)`;
 
             const mins = now.getMinutes();
-            // const minsDegrees = ((mins / 60 )*360)+90;
             const minsDegrees = mins*6;
+                // ${minsDegrees} is a placeholder where the value of the variable minsDegrees will be inserted into the string
             minsHand.style.transform = `rotate(${minsDegrees}deg)`;
 
             const hour = now.getHours();
-            // const hourDegrees = ((mins/12) * 360)+90;
             const hourDegrees = hour*30 + mins/2;
             hourHand.style.transform = `rotate(${hourDegrees}deg)`;
 
